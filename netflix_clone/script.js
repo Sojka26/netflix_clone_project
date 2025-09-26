0eb213699727eb27f2c75ed05520e47881e21a01
Sojka26 <mara1x@email.cz>
2025-09-26 11:28:01 +0200

    Update script.js

--- netflix_clone/script.js
+++ netflix_clone/script.js
@@ -1,4 +1,4 @@
-// script.ts – SPA s rozšířeným vyhledáváním a offline fallbackem na lokální postery
+// script.ts – netflix clone
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
